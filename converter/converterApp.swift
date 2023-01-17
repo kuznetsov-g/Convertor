47fedd0768037dc54b4eef83db54cec89b5b0dd2
George <backs159@icloud.com>
2023-01-17 01:51:16 -0500

    Initial Commit

---
+++ converter/converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  converterApp.swift
+//  converter
+//
+//  Created by Георгий Кузнецов on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
